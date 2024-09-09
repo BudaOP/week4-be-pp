24407410c9fcf07636ce61444894b2051b8ae989
BudaOP <ibudanov24@gmail.com>
2024-09-09 14:30:00 +0300

    First update

---
+++ week4-be-pp/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const tourRouter = require("./routes/tourRouter");
+const userRouter = require("./routes/userRouter");
+// Middleware to parse JSON
+app.use(express.json());
+// Use the tourRouter for all /tours routes
+app.use("/tours", tourRouter);
+app.use("/users", userRouter);
+
+const port = 4000;
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
